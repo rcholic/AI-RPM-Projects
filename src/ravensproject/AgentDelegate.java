648adb4ff68661b4352994ef80118b6c1245970e
rcholic <ivytony@gmail.com>
2015-07-18 10:18:38 -0400

    tie solver in 3x3

--- src/ravensproject/AgentDelegate.java
+++ src/ravensproject/AgentDelegate.java
@@ -52,7 +52,9 @@ public class AgentDelegate {
         }
     }
 
-    public AgentDelegate() {}
+    public AgentDelegate() {
+    }
+
     public AgentDelegate(RavensProblem ravensProblem) {
         this.ravensProblem = ravensProblem;
     }
@@ -84,12 +86,12 @@ public class AgentDelegate {
             ravensFigure2 = ravensProblem.getFigures().get(Integer.toString(i));
             rfTransformation2 = new RFTransformation(ravensFigure1, ravensFigure2);
 
-                int scoreCurrRFTransform = rfTransformation1.scoreRFTransformations(rfTransformation2);
-                System.out.println("scoreCurrRFTransformation = " + scoreCurrRFTransform + ", maxRFScore = " + maxRFTransformScore);
-                if (scoreCurrRFTransform >= maxRFTransformScore) {  //what if equal ?? -- how to break tie??
-                    this.answerChoice = i;
-                    maxRFTransformScore = scoreCurrRFTransform;
-                }
+            int scoreCurrRFTransform = rfTransformation1.scoreRFTransformations(rfTransformation2);
+            System.out.println("scoreCurrRFTransformation = " + scoreCurrRFTransform + ", maxRFScore = " + maxRFTransformScore);
+            if (scoreCurrRFTransform >= maxRFTransformScore) {  //what if equal ?? -- how to break tie??
+                this.answerChoice = i;
+                maxRFTransformScore = scoreCurrRFTransform;
+            }
         }
 
         int correctAnswer = ravensProblem.checkAnswer(this.answerChoice);
@@ -133,15 +135,15 @@ public class AgentDelegate {
         rfTransformationScores[5] = rfTransformation1.scoreRFTransformations(rfTransformation3);
 
 
-
         ravensFigure1 = ravensProblem.getFigures().get("G");
         ravensFigure2 = ravensProblem.getFigures().get("H");
         rfTransformation1 = new RFTransformation(ravensFigure1, ravensFigure2);
 
 
         int[] answerTransformScores;
-        double similarityScore = Integer.MAX_VALUE;
+        int similarityScore = Integer.MAX_VALUE;
         System.out.println("first two row scores: " + Arrays.toString(rfTransformationScores));
+        int[][] answerScoresMatrix = new int[8][3]; //8 answers, 3 RFtransformation scores
         for (int i = 1; i <= 8; i++) {
             answerTransformScores = new int[3];
             ravensFigure3 = ravensProblem.getFigures().get(Integer.toString(i));
@@ -151,15 +153,22 @@ public class AgentDelegate {
             answerTransformScores[0] = rfTransformation1.scoreRFTransformations(rfTransformation2);
             answerTransformScores[1] = rfTransformation2.scoreRFTransformations(rfTransformation3);
             answerTransformScores[2] = rfTransformation1.scoreRFTransformations(rfTransformation3);
+            answerScoresMatrix[i-1] = answerTransformScores;
 
             System.out.println("third row scores: " + Arrays.toString(answerTransformScores));
 
-            double curSimilarityScore = getSimilarityScores(answerTransformScores);
+            int curSimilarityScore = getSimilarityScores(answerTransformScores);
             System.out.println("curSimilarityScore = " + curSimilarityScore + ", previous similarityScore = " + similarityScore);
             if (similarityScore > curSimilarityScore) {  //how to handle tie??
                 similarityScore = curSimilarityScore;
                 this.answerChoice = i;
             }
+
+            else if (similarityScore == curSimilarityScore) {
+                solve3x3RPMTie(ravensProblem);
+//                breakTie(this.answerChoice, i, answerScoresMatrix);
+            }
+
         }
 
         int correctAnswer = ravensProblem.checkAnswer(this.answerChoice);
@@ -170,8 +179,75 @@ public class AgentDelegate {
     }
 
 
+    public void solve3x3RPMTie(RavensProblem ravensProblem) {
+        initTransformContainers();
+
+        ravensFigure1 = ravensProblem.getFigures().get("A");
+        ravensFigure2 = ravensProblem.getFigures().get("D");
+        ravensFigure3 = ravensProblem.getFigures().get("G");
+        rfTransformation1 = new RFTransformation(ravensFigure1, ravensFigure2);
+        // int numTrans = rfTransformation1.compileROTransformationsInMatchedObjects().size();
+
+        rfTransformation2 = new RFTransformation(ravensFigure2, ravensFigure3);
+        // numTrans += rfTransformation2.compileROTransformationsInMatchedObjects().size();
+
+        rfTransformation3 = new RFTransformation(ravensFigure1, ravensFigure3);
+        rfTransformationScores[0] = rfTransformation1.scoreRFTransformations(rfTransformation2);
+        rfTransformationScores[1] = rfTransformation2.scoreRFTransformations(rfTransformation3);
+        rfTransformationScores[2] = rfTransformation1.scoreRFTransformations(rfTransformation3);
+
+
+        ravensFigure1 = ravensProblem.getFigures().get("B");
+        ravensFigure2 = ravensProblem.getFigures().get("E");
+        ravensFigure3 = ravensProblem.getFigures().get("H");
+        rfTransformation1 = new RFTransformation(ravensFigure1, ravensFigure2);
+        //numTrans = rfTransformation1.compileROTransformationsInMatchedObjects().size();
+
+        rfTransformation2 = new RFTransformation(ravensFigure2, ravensFigure3);
+        //numTrans += rfTransformation2.compileROTransformationsInMatchedObjects().size();
+
+        rfTransformation3 = new RFTransformation(ravensFigure1, ravensFigure3);
+
+        rfTransformationScores[3] = rfTransformation1.scoreRFTransformations(rfTransformation2);
+        rfTransformationScores[4] = rfTransformation2.scoreRFTransformations(rfTransformation3);
+        rfTransformationScores[5] = rfTransformation1.scoreRFTransformations(rfTransformation3);
+
+
+        ravensFigure1 = ravensProblem.getFigures().get("C");
+        ravensFigure2 = ravensProblem.getFigures().get("F");
+        rfTransformation1 = new RFTransformation(ravensFigure1, ravensFigure2);
+
+
+        int[] answerTransformScores;
+        int similarityScore = Integer.MAX_VALUE;
+        System.out.println("first two row scores: " + Arrays.toString(rfTransformationScores));
+        int[][] answerScoresMatrix = new int[8][3]; //8 answers, 3 RFtransformation scores
+        for (int i = 1; i <= 8; i++) {
+            answerTransformScores = new int[3];
+            ravensFigure3 = ravensProblem.getFigures().get(Integer.toString(i));
+            rfTransformation2 = new RFTransformation(ravensFigure2, ravensFigure3);
+            rfTransformation3 = new RFTransformation(ravensFigure1, ravensFigure3);
+
+            answerTransformScores[0] = rfTransformation1.scoreRFTransformations(rfTransformation2);
+            answerTransformScores[1] = rfTransformation2.scoreRFTransformations(rfTransformation3);
+            answerTransformScores[2] = rfTransformation1.scoreRFTransformations(rfTransformation3);
+            answerScoresMatrix[i-1] = answerTransformScores;
+
+            System.out.println("third row scores: " + Arrays.toString(answerTransformScores));
+
+            int curSimilarityScore = getSimilarityScores(answerTransformScores);
+            System.out.println("curSimilarityScore = " + curSimilarityScore + ", previous similarityScore = " + similarityScore);
+            if (similarityScore >= curSimilarityScore) {  //how to handle tie??
+                similarityScore = curSimilarityScore;
+                this.answerChoice = i;
+            }
+        }
+    }
+
+
     /**
      * for 3x3 RPM, the similarity score is the smaller, the more similar it is!
+     *
      * @param answerTransformScores
      * @return
      */
@@ -181,7 +257,7 @@ public class AgentDelegate {
         int[] thirdRowNum = answerTransformScores;
         for (int i = 0; i < 3; i++) {
             firstRowNum[i] = rfTransformationScores[i];
-            secondRowNum[i] = rfTransformationScores[i+3];
+            secondRowNum[i] = rfTransformationScores[i + 3];
         }
         int minSimilarScore = 0;
         for (int i = 0; i < 3; i++) {
@@ -210,11 +286,42 @@ public class AgentDelegate {
 
     private void initTransformContainers() {
         this.rfTransformationScores = new int[6];
-       // this.answerRFTransformationScores = new ArrayList<>();
+        // this.answerRFTransformationScores = new ArrayList<>();
 //        this.numAnswerTransformations = new ArrayList<>();
         this.transformationsWithAnswerFigs = new ArrayList<>();
         this.answerChoice = 6; //initialized to 6
     }
+
+    private void breakTie(int answerChoice, int tie, int[][] answerScoresMatrix) {
+        int[] firstRowNum = new int[3];
+        int[] secondRowNum = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            firstRowNum[i] = rfTransformationScores[i];
+            secondRowNum[i] = rfTransformationScores[i + 3];
+        }
+
+        int[] answerChoiceScores = answerScoresMatrix[answerChoice - 1];
+        int[] tieChoiceScores = answerScoresMatrix[tie - 1];
+        boolean pattern11 = firstRowNum[1] - firstRowNum[0] < 0;
+        boolean pattern12 = firstRowNum[2] - firstRowNum[1] < 0;
+        boolean pattern21 = secondRowNum[1] - secondRowNum[0] < 0;
+        boolean pattern22 = secondRowNum[2] - secondRowNum[1] < 0;
+        boolean combinedPattern1 = pattern11 && pattern21;
+        boolean combinedPattern2 = pattern12 && pattern22;
+        boolean ansPattern31 = answerChoiceScores[1] - answerChoiceScores[0] < 0;
+        boolean ansPattern32 = answerChoiceScores[2] - answerChoiceScores[1] < 0;
+        boolean tiePattern41 = tieChoiceScores[1] - tieChoiceScores[0] < 0;
+        boolean tiePattern42 = tieChoiceScores[2] - tieChoiceScores[1] < 0;
+
+        if (combinedPattern1 == ansPattern31 && combinedPattern2 == ansPattern32) {
+            this.answerChoice = answerChoice;
+        }
+
+        this.answerChoice = tie;
+    }
+
+
 
     public static void main(String[] args) {
         List<Integer> myList = new ArrayList<>();
@@ -224,9 +331,9 @@ public class AgentDelegate {
         myList.add(-9);
         myList.add(0);
 //        myList.remove(10);
-      //  System.out.println(myList);
+        //  System.out.println(myList);
         Collections.sort(myList);
-      //  System.out.println(myList);
+        //  System.out.println(myList);
     }
 
 }
