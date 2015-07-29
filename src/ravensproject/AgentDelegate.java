1f5462ae12626e8af1bb3c19d97b5c9c407eb3bc
rcholic <ivytony@gmail.com>
2015-07-29 14:23:42 -0400

    removed reference code

--- src/ravensproject/AgentDelegate.java
+++ src/ravensproject/AgentDelegate.java
@@ -71,13 +71,9 @@ public class AgentDelegate {
         } else if (this.ravensProblem.getProblemType().contains("3x3") && this.ravensProblem.hasVerbal()) {
             solve3x3RPM(ravensProblem);
         } else if (this.ravensProblem.getProblemType().contains("2x2") && this.ravensProblem.hasVisual()) {
-            //RavensProblem describedProblem = imageSolver.solve2x2RPMVisually(ravensProblem);
-            //solve2x2RPM(describedProblem);
-            this.answerChoice = imageProcessor.solveByPixelCounts(ravensProblem);
+              this.answerChoice = imageProcessor.solveByPixelCounts(ravensProblem);
             getAnswerChoice();
         } else if (this.ravensProblem.getProblemType().contains("3x3") && this.ravensProblem.hasVisual()) {
-           // RavensProblem describedProblem = imageSolver.solve3x3RPMVisually(ravensProblem);
-           // solve3x3RPM(describedProblem);
             this.answerChoice = imageProcessor.solveByPixelCounts(ravensProblem);
             getAnswerChoice();
         }
